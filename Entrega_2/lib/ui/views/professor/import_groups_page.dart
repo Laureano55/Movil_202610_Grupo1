e76ab6a48ededc764c1fb1a9b633ea7ad45ed569
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2026-03-25 00:05:14 -0500

    Add ImportGroupsPage for CSV group import functionality

---
+++ Entrega_2/lib/ui/views/professor/import_groups_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../viewmodels/csv_import_controller.dart';
+
+class ImportGroupsPage extends StatelessWidget {
+
+  final CsvImportController controller = Get.put(CsvImportController());
+
+  final String courseId = "movil_course";
+
+  ImportGroupsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Importar CSV de grupos"),
+      ),
+      body: Center(
+        child: Obx(() {
+
+          if (controller.isUploading.value) {
+            return const CircularProgressIndicator();
+          }
+
+          return ElevatedButton(
+            onPressed: () {
+              controller.uploadCSV(courseId);
+            },
+            child: const Text("Seleccionar y subir CSV"),
+          );
+
+        }),
+      ),
+    );
+  }
+}
